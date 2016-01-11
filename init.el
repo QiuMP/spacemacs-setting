20a70b7fe1aa344c26e5061c39a78980d85f52e0
QiuMP <qiumupo@live.com>
2016-01-11 22:03:02 +0800

    test

--- init.el
+++ init.el
@@ -220,6 +220,7 @@ layers configuration. You are free to put any user code."
    (global-company-mode)
    (linum-relative-global-mode)
    (global-git-commit-mode)
+   test
 
    (setq default-input-method 'xh)
    (spacemacs//set-monospaced-font "Consolas for Powerline" "Microsoft YaHei" 17 18)
